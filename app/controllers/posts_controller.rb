dc8eb4ed30621fbcc9c78c281605bf546d915589
chibi0310mayu <ilove0910bio@gmail.com>
2021-03-20 17:11:52 +0900

    first commit

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,16 @@
+class PostsController < ApplicationController
+
+  def index  # indexアクションを定義
+    @posts = Post.all  # 全てのレコードを@postに代入
+    
+  end
+
+  def new
+    
+  end
+
+  def create
+    Post.create(content: params[:content])
+    
+  end
+end
